2b7e6bd50d75226cfd84dfb1d4ee757f0f2a6b2a
bugbasher13 <da.leveltester@gmail.com>
2024-07-14 18:38:40 +0300

    Telegram bot has been added.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Hellow World')
+from os import environ
+
+import telebot
+
+if  __name__ == "__main__":
+    print('Hellow World')
+    bot = telebot.TeleBot(environ.get("TELEBOT_TOKEN"))
+
+    @bot.message_handler(commands=['ping'])
+    def ping_command(message):
+        bot.send_message(message.chat.id, 'pong')
+
+
+    bot.infinity_polling(timeout=20, long_polling_timeout=45)
